1ceee15debeb38cd7bb560b0c16ffa57b19eacd7
Neomill <rneomillmarc@gmail.com>
2022-01-05 05:29:52 +0800

    rename script for accordion and place the file appropriately

---
+++ js/accordion_functionality.js
@@ -0,0 +1,18 @@
+const accordionHeaders = document.querySelectorAll('[data-accordion-header]');
+let cacheElement = null;
+
+Array.prototype.forEach.call(accordionHeaders, accordionHeader => {
+let target = accordionHeader.parentElement.nextElementSibling;
+  accordionHeader.onclick = () => {
+    if (cacheElement !== null && cacheElement !== accordionHeader){
+      cacheElement.setAttribute('aria-expanded',false)
+      cacheElement.parentElement.nextElementSibling.hidden = true;
+    }
+
+    let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
+    accordionHeader.setAttribute('aria-expanded', !expanded);
+    target.hidden = expanded;
+
+    cacheElement = accordionHeader;
+  }
+})
